fad1f46ea2f9b4eb3d0419cbb7035de3f744d66f
Mayowa Aj <mayowaaj69@hotmail.co.uk>
2019-03-04 05:31:39 +0100

    Fileimage uses a callable for concurrency

--- src/genie/FileImage.java
+++ src/genie/FileImage.java
@@ -5,10 +5,36 @@
  */
 package genie;
 
+import Interfaces.IJSON;
+import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
 /**
  * Stores a mirror of the file system
  * @author mayowa
  */
-public class FileImage {
+public class FileImage implements IJSON {
+    
+    private File root;
+    
+    private FileImage(String pathToRoot) {
+        this.root = new File(pathToRoot);
+        //TODO create fileimage
+    }
+    
+    public static Future<FileImage> make(String pathToRoot) {
+        
+        Callable<FileImage> callable = () -> {
+            return new FileImage(pathToRoot);
+        };
+        Future<FileImage> future = Store.getExecutor().submit(callable);
+        return future; 
+    }
+
+    @Override
+    public String toJson() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
